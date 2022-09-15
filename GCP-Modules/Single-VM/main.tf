31cd2c25085717b93f4949cc609297c0e3e4c083
sumitvermadevops <113206234+sumitvermadevops@users.noreply.github.com>
2022-09-15 17:20:10 -0400

    update 4

---
+++ GCP-Modules/Single-VM/main.tf
@@ -0,0 +1,24 @@
+#Terrafor provider
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "4.35.0"
+    }
+  }
+}
+
+#call network module
+module "network" {
+  source       = "../modules/test/network"
+  network_name = "sumit-network"
+}
+
+#Call simple instance module
+module "simple" {
+  source          = "../modules/test/instance"
+  vm_name         = "sumit-instance-1"
+  network_name    = module.network.network_name
+  subnetwork_name = module.network.subnetwork_name
+}
+
